81bb71a4ad33079628c5b312465daa455bf091de
soolo <gimsooro@gmail.com>
2020-04-10 14:29:14 -0400

    add bubble sort explanation

--- demos/c/include/sort/bubblesort.h
+++ demos/c/include/sort/bubblesort.h
@@ -3,6 +3,15 @@
 
 /*
     Sorts given array by bubblesort.
+
+    Bubble Sort Explanation:
+    1. Start at 0
+    2. Compare current value to next value
+        - If current value is larger than next value, swap
+    3. Move down list by 1
+    4. Repeat steps 2 and 3 until hit the list size - 1 (since you test next value)
+    5. Repeat steps 1 to 4 again but up to the list size - number of loops - 1
+    6. Once the list size - number of loops - 1 reaches 0, stop.
 */
 void bubbleSort(void ** toSort, int len, int (*compareFunc)(void * obj1, void * obj2));
 
